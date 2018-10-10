bc76e48e10d1b2996c7e1bb49bb253a254c8801f
Katelyn <katelyn.rimkunas@gmail.com>
2018-10-10 14:18:27 -0600

    playing with html -- tests in progress

---
+++ spec/features/user_sees_all_books_spec.rb
@@ -0,0 +1,21 @@
+require "rails_helper"
+
+describe 'Books main page' do
+
+  before(:each) do
+    author = Author.create(name: "Author 1", )
+
+  end
+
+  describe 'User sees a book with details' do
+
+  end
+
+  describe 'User sees all books' do
+  end
+
+
+  describe 'User sees a filtered set of books' do
+  end
+
+end
